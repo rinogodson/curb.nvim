7278902a773d0fcd06521d9c08af4410f8bdf627
Rino Godson <rinogodson@gmail.com>
2026-05-28 22:42:03 +0530

    added some stuff to this website, it's not empty (now) and will be an amazing minimal website

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script>
+  import Button from './components/Button.svelte'
 </script>
 
-<div class="bg-black text-white">
-This is the start of this website
+<div class="bg-black text-white flex flex-col items-center justify-center min-h-screen gap-4">
+  This is the start of this website
+  <Button label="Get Started" link="/getting-started" />
 </div>
---
+++ src/components/Button.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let label = ''
+  export let link = ''
+</script>
+
+<a href={link} class="inline-flex items-center justify-center bg-[#232323] text-[#D6D6D6] font-['Pixelify_Sans']">
+  {label}
+</a>
